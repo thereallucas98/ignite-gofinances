0255b3fafbe0ab233bef43a07ca7dafa1dd2f4b5
thereallucas98 <lucas1998david@gmail.com>
2021-10-26 20:37:16 -0300

    Criando Componentes com o Styled Components

--- App.tsx
+++ App.tsx
@@ -1,22 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import React from "react";
+
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title="Ignite" />
-      <StatusBar style="auto" />
-    </View>
+    <Dashboard />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
