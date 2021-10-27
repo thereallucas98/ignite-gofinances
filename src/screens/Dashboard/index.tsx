35a53c7214d47c74311d9599598bb2d9059dbfb8
thereallucas98 <lucas1998david@gmail.com>
2021-10-27 17:33:56 -0300

    Utilizando Proporçoes em Medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>
-        Dashboard Screen!
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/thereallucas98.png" }} />
+            <User>
+              <UserGreeeting>Olá, </UserGreeeting>
+              <UserName>David Lucas</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
