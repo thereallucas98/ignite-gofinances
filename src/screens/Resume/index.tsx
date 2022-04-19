077e8d52d0696237cff14ea8f06ba31f2a8378b5
thereallucas98 <lucas1998david@gmail.com>
2022-04-19 20:54:12 -0300

    Resume

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+export function Resume() {
+  return (
+    <Container>
+      <Text>Resume</Text>
+    </Container>
+  );
+};
