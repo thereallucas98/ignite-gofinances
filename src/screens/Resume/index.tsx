213186617acc6749c2c81b3f78377d6c10f3c6f8
thereallucas98 <lucas1998david@gmail.com>
2022-04-20 18:06:32 -0300

    Criando a interface de grafico

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,11 +1,15 @@
-import { Text } from 'react-native';
-
-import { Container } from './styles';
+import React from "react";
+import { HistoryCard } from "../../components/HistoryCard";
+import { Container, Header, Title } from './styles';
 
 export function Resume() {
   return (
     <Container>
-      <Text>Resume</Text>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+
+      <HistoryCard title="Compras" amount="R$ 150,50" color="red" />
     </Container>
   );
 };
