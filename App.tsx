2fdbebce6ba72be33d65c6520e487f304c896db5
thereallucas98 <lucas1998david@gmail.com>
2022-04-22 07:53:10 -0300

    Interface de Autenticaçao & Tipando a importaçao do SVG

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import theme from "./src/global/styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./src/routes/app.routes";
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -35,7 +36,8 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        {/* <AppRoutes /> */}
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
